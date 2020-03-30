cfea8ec427743966bf3b5e8ee89411f73dd8b109
Solti Gábor <gabor.solti@freemail.hu>
2020-03-30 13:24:26 +0200

    Név és jelszó sorok hozzáadva

--- Codes/MarketClient/src/main/java/marketplace/client/LoginScreen.java
+++ Codes/MarketClient/src/main/java/marketplace/client/LoginScreen.java
@@ -3,6 +3,7 @@ package marketplace.client;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -10,21 +11,30 @@ import javafx.stage.Stage;
 public class LoginScreen extends Application {
     private LoginController controller = new LoginController(this);
 
-
+    TextField nameField;
+    TextField passwordField;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        nameField = new TextField();
+        passwordField = new TextField();
 
         HBox nameLine = new HBox();
-        nameLine.getChildren().add(new Label("NÈv:"));
+        nameLine.getChildren().add(new Label("N√©v:"));
+        nameLine.getChildren().add(nameField);
 
+        HBox passwordLine = new HBox();
+        passwordLine.getChildren().add(new Label("Jelsz√≥:"));
+        passwordLine.getChildren().add(passwordField);
 
         VBox root = new VBox();
         root.getChildren().add(nameLine);
+        root.getChildren().add(passwordLine);
+
         Scene scene = new Scene(root, 300, 250);
 
-        primaryStage.setTitle("BelÈpÈs a piacra");
+        primaryStage.setTitle("Bel√©p√©s a piacra");
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
