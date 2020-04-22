e86b7ff2a7472791296712196792a108bdde8dfb
Takács Ákos <akos9511@gmail.com>
2020-04-22 13:12:51 +0200

    Javítás elkezdve

--- Codes/Marketplace/src/main/java/marketplace/currencyexchange/CurrencyExchange.java
+++ Codes/Marketplace/src/main/java/marketplace/currencyexchange/CurrencyExchange.java
@@ -1,28 +1,25 @@
 package marketplace.currencyexchange;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.incubator.http.HttpClient;
-import jdk.incubator.http.HttpRequest;
-import jdk.incubator.http.HttpResponse;
+import org.eclipse.jetty.server.Response;
+
 
 public class CurrencyExchange
 {
     public double getExchangeRates(int price, String currency) throws IOException, InterruptedException, JsonMappingException {
         double newPrice = 0;
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.exchangeratesapi.io/latest")).build();
+        final String REST_URI = "https://api.exchangeratesapi.io/latest";
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandler.asString());
-
-        String responseString = response.body();
+        Response response = client.target(REST_URI).request(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).get;
 
         ObjectMapper mapper = new ObjectMapper();
-        CurrencyExchangeData exchangeRates = mapper.readValue(responseString, CurrencyExchangeData.class);
+        CurrencyExchangeData exchangeRates = mapper.readValue(response.readEntity(String.class), CurrencyExchangeData.class);
 
         switch (currency)
         {
