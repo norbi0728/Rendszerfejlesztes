584ca765d65c7edc99c66b54e40fc9726f426a2b
Radákovits Norbert <radakovitsn007@gmail.com>
2020-05-18 16:44:49 +0200

    updateUserStatistic(String userName, String category, String event) a logikában továbbfejlesztve commentben, hogy melyik eseményt mikor kell meghívni.

--- Codes/Marketplace/src/main/java/marketplace/logic/UserManagement.java
+++ Codes/Marketplace/src/main/java/marketplace/logic/UserManagement.java
@@ -2,7 +2,14 @@ package marketplace.logic;
 
 import marketplace.database.Database;
 import marketplace.model.PersonalInformation;
+import marketplace.model.Statistics;
 import marketplace.model.User;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UserManagement {
     static void ki(Object a){
@@ -34,11 +41,61 @@ public class UserManagement {
         return "The user does not exist!";
     }
 
-    public String updateUserStatistic(String userName){
+    /*
+    * Implemented events:
+    * onList (when list by category),
+    * onClick (when click on a listing),
+    * onBid (when make a bid),
+    * onAddListing (when create a listing),
+    * onWin (when successfully wins an auction),
+    * onDidNotGet (when made a bid on an auction but lost it)*/
+    public String updateUserStatistic(String userName, String category, String event){
         Database db = Database.getDatabase();
-        if (db.userExists(userName)){
-            db.updateUserStatistics(db.getUser(userName));
-            return "Done";
+        List<String> categories = Arrays.asList("Electrical",
+                "Sport", "Vehicle and Parts", "Beautycare",
+                "Cultural", "Home", "Gathering");
+        if (db.userExists(userName)) {
+            if (categories.contains(category)) {
+                User me = db.getUser(userName);
+                Map<String, Double> temp = new HashMap<>();
+                switch (event) {
+                    case "onList":
+                        temp = me.getStatistics().getStats();
+                        temp.put(category, temp.get(category) + 0.1);
+                        me.getStatistics().setStat(temp);
+                        break;
+                    case "onClick":
+                        temp = me.getStatistics().getStats();
+                        temp.put(category, temp.get(category) + 0.2);
+                        me.getStatistics().setStat(temp);
+                        break;
+                    case "onBid":
+                        temp = me.getStatistics().getStats();
+                        temp.put(category, temp.get(category) + 0.4);
+                        me.getStatistics().setStat(temp);
+                        break;
+                    case "onAddListing":
+                        temp = me.getStatistics().getStats();
+                        temp.put(category, temp.get(category) + 0.5);
+                        me.getStatistics().setStat(temp);
+                        break;
+                    case "onWin":
+                        temp = me.getStatistics().getStats();
+                        temp.put(category, temp.get(category) + 1);
+                        me.getStatistics().setStat(temp);
+                        break;
+                    case "onDidNotGet":
+                        temp = me.getStatistics().getStats();
+                        temp.put(category, temp.get(category) + 1.2);
+                        me.getStatistics().setStat(temp);
+                        break;
+                    default:
+                        return "There is no such event yet!";
+                }
+                db.updateUserStatistics(me);
+                return "Done";
+            }
+            return "No such a category in the database!";
         }
         return "There is no such a user in the database!";
     }
